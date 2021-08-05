49854030b07d48fc0c8e990c6c4024398442868f
Carlos Eduardo Rugeles Lache <rugeles10@gmail.com>
2021-08-04 21:25:40 -0500

    Fix paths

--- services/dnaStorageFactory.js
+++ services/dnaStorageFactory.js
@@ -1,8 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
-const {getConnection} = require('dbConnection');
+const {getConnection} = require('./dbConnection');
 const DB_URL = "mongodb://localhost:27017/meli"
-const dnaStorage = require('dnaStorage')
+const dnaStorage = require('./dnaStorage')
 module.exports = () => {
     const connection = getConnection(mongoose,DB_URL, {})
     return dnaStorage(connection)
